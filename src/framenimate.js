383708e825924a0f21a9705786dc5d196104684c
Vitor Margis <vitor.margis@gmail.com>
2016-07-04 11:08:59 -0300

    Feature - add reverse option

--- src/framenimate.js
+++ src/framenimate.js
@@ -1,39 +1,51 @@
 (function() {
+  var defaults = {
+    speed: 50,
+    smooth: false,
+    reverse: false,
+    smoothFactor: 0,
+  };
 
-  var frame,frames,framenimateCSS,framenimateSmoothFactor,framenimateAtt,framenimateConfig;
-
-  this.Framenimate = function(element, config){
+  this.Framenimate = function(element, config) {
     framenimateWrapper = [];
     framenimateWrapper = element || document.querySelectorAll('[framenimate]');
 
-    for(var index in framenimateWrapper){
-      if(+index >= 0){
-        frame = [];
-        frame[index] = -1
-        framenimateConfig = [];
+    for(var index in framenimateWrapper) {
+      if(+index >= 0) {
+        var frame = [];
+        var framenimateCSS = []
+        var framenimateConfig = [];
+
+        frame[index] = 0
         framenimateConfig[index] = {};
-        framenimateCSS = []
 
-        if(!element) framenimateAttr = framenimateWrapper[index].getAttribute('framenimate').toString().split(';');
+        if(!element) var framenimateAttr = framenimateWrapper[index].getAttribute('framenimate').toString().split(';');
 
-
-        if(framenimateWrapper[index].getAttribute('framenimate')){
+        if(framenimateWrapper[index].getAttribute('framenimate')) {
           for (var attr = 0; attr < framenimateAttr.length; attr++) {
-            if(framenimateAttr[attr] !== ''){
+            if(framenimateAttr[attr] !== '') {
               var entry = framenimateAttr[attr].replace(' ', '').split(':');
               framenimateConfig[index][entry.splice(0,1)[0]] = entry.join(':').replace(' ', '');
             }
           }
         }
 
-        if(framenimateConfig[index].smooth){
-          framenimateSmoothFactor = framenimateConfig[index].smoothFactor || framenimateConfig[index].speed/1000 || defaults.speed/1000;
+        if(framenimateConfig[index].smooth) {
+          var framenimateSmoothFactor = framenimateConfig[index].smoothFactor || framenimateConfig[index].speed/1000 || defaults.speed/1000;
           framenimateCSS[index] = "transition: opacity " + framenimateSmoothFactor + "s ease; position: absolute; opacity: 0";
         } else {
           framenimateCSS[index] = "position: absolute; opacity: 0";
         }
 
-        frames = framenimateWrapper[index].children;
+        // convert nodelist into an array
+        var framenimateOrder = framenimateConfig[index].reverse || defaults.reverse;
+
+        if(framenimateOrder.toString() === 'true') {
+          var frames = Array.prototype.slice.call(framenimateWrapper[index].children).reverse();
+        } else {
+          var frames = Array.prototype.slice.call(framenimateWrapper[index].children);
+        }
+
         for (var i = 0; i < frames.length; ++i) {
           frames[i].setAttribute('style', framenimateCSS[index]);
         }
@@ -43,25 +55,19 @@
     }
   };
 
-  Framenimate.prototype.loop = function(index, frame, frames, framenimateConfig, framenimateCSS){
+  Framenimate.prototype.loop = function(index, frame, frames, framenimateConfig, framenimateCSS) {
     frame[index] <= frames.length-2 ? frame[index]++ : frame[index] = 0;
 
-    function removeFrameCurrent(){
+    function removeCurrentFrame() {
       if(frame[index] !== 0) frames[frame[index]-1].setAttribute('style', framenimateCSS[index]);
       if(frame[index] === 0) frames[frames.length-1].setAttribute('style', framenimateCSS[index]);
     }
 
-    setTimeout(removeFrameCurrent, framenimateConfig[index].speed/2 || defaults.speed/2);
+    setTimeout(removeCurrentFrame, framenimateConfig[index].speed/2 || defaults.speed/2);
     frames[frame[index]].setAttribute('style', framenimateCSS[index].replace('opacity: 0', 'opacity: 1'));
   };
 
-  var defaults = {
-    speed: 50,
-    smooth: false,
-    smoothFactor: 0,
-  };
-
-  if(document.querySelectorAll('[framenimate]').length !== 0){
+  if(document.querySelectorAll('[framenimate]').length !== 0) {
     this.framenimation = new Framenimate();
   }
 })();
